65bbd1cd7ac3d2e4d009b1b01129ec1f4235c0ad
Sumit yogi <yogisumit97@gmail.com>
2026-01-24 10:06:40 +0530

    IO in cpp

--- 1_Introduction.cpp
+++ 1_Introduction.cpp
@@ -6,21 +6,26 @@ Bjarne stroustrup in 1979
 
 c++ is -> statically typed, free form, compiled , general purpose language.
 gaming engines, embedded, software development, speed of execution(less overhead)
+
+// --> used for single line comments. used for user to understsand what is going on.
 */
 
-
-
+/*all files in this project will have function name : XXXX_main() >> this is our main function
+ *if you want to run the program XXXX_main() should be updated to main() only.
+ *as there can be only 1 main thus this is being done.
+*/
 
 #include <iostream> // header file - include relevant functionalities
 using namespace std; //std = standard namespace, in our code cout is declared here
 
-int main() { // function - returns integer. one main function only in a program.
+int INTRO_main() { // function - returns integer. one main function only in a program.
     // os searches for main to start program.
 
     cout << "First program !!" << endl; // output to console.
+    // "First Program !!" ----> character string
     // cout - object in std lib, << - stream insertion operator
+
     return 0; //returning integer- 0 means successful execution.
 }
-
 
 
---
+++ 2_InputAndOutput.cpp
@@ -0,0 +1,42 @@
+/* IO :
+cin = console input
+cout = console output
+
+cin and cout both are objects of istream and ostream class respectively,
+these 2 classes reside inside istream and ostream header files - merged as iostream.
+
+cin is used with >> (stream extraction operator)
+cout is used with << (stream insertion operator)
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int IO_main() {
+    string name; // string datatype, name is variable to store string type value.
+    cin >> name; // cin stops reading at whitespaces(space, tab, newline)
+    // to read full line use getline(cin, name);
+    //cin >> → token-based input
+    //getline → line-based input
+
+    cout << "Hello " << name << endl; // printing on console
+    // endl is used for newline : as cout uses stream only we have to tell when to change line.
+    int i=0, j=0; // interger type variables i and j initialized with 0 value.
+    // 2 integers - and addition
+    cin >> i >> j; // user input
+    cout << "Addition for "<<i <<" and "<<j<< " is "<< i+j <<endl;
+    // i+j is expression whereas "i+j" -- will be string
+    //'i+j' -- gives garbage values, as it is used with one char only.
+    return 0;
+}
+
+
+/*Trap :
+When mixing formatted input (cin >>) with line-based input (getline()),
+unexpected empty strings may be read.
+This occurs because cin >> extracts only the required token
+(e.g., an integer or word) and leaves the trailing newline character ('\n')
+in the input buffer. When getline() is called immediately afterward,
+it encounters this leftover newline and terminates without reading the intended line.
+*/

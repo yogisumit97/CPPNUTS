e1e5bf78e1c09844862e47c972018c67781237d3
Sumit yogi <yogisumit97@gmail.com>
2026-01-24 09:22:44 +0530

    Introduction and first Program. (Revising from basics)

---
+++ 1_Introduction.cpp
@@ -0,0 +1,19 @@
+/*
+OOPS language : anstraction, encapsulation, inheritance, polymorphism
+but not pure oops ? -- c style functional/ procedural programming too.
+extension to c
+Bjarne stroustrup in 1979
+
+c++ is -> statically typed, free form, compiled , general purpose language.
+gaming engines, embedded, software development, speed of execution(less overhead)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<< "First program !!" << endl;
+}
+
+
+

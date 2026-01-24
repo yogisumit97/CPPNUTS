af46110eb196ccb866ffa1c6f8c92db5d0d5141d
Sumit yogi <yogisumit97@gmail.com>
2026-01-24 09:30:24 +0530

    Introduction and first Program. (Making repository with notes so that anyone can learn using this)

--- 1_Introduction.cpp
+++ 1_Introduction.cpp
@@ -8,12 +8,18 @@ c++ is -> statically typed, free form, compiled , general purpose language.
 gaming engines, embedded, software development, speed of execution(less overhead)
 */
 
-#include <iostream> // header file - include relevant functionalities
-using namespace std; //std = standard namespace
 
-int main() {
-    cout<< "First program !!" << endl;
-    return 0;
+
+
+#include <iostream> // header file - include relevant functionalities
+using namespace std; //std = standard namespace, in our code cout is declared here
+
+int main() { // function - returns integer. one main function only in a program.
+    // os searches for main to start program.
+
+    cout << "First program !!" << endl; // output to console.
+    // cout - object in std lib, << - stream insertion operator
+    return 0; //returning integer- 0 means successful execution.
 }
 
 

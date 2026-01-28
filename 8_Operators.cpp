3531b9262bc68af41a616d16ad9a7f35be14abb2
Sumit yogi <yogisumit97@gmail.com>
2026-01-28 11:39:27 +0530

    C++ Learning

--- 8_Operators.cpp
+++ 8_Operators.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
+class Base {
 
+};
+class Base2 {
+    int x;
+};
+class Base3 {
+    int x;
+    char y;
+    bool z;
+    int p;
+};
+void fun(char arr[]) {
+    cout<< sizeof(arr) << endl;
+}
 int main() {
     // Relational operators : ==, !=, >, >=, <, <=
     string s1="c++", s2="c++";
@@ -36,5 +50,55 @@ int main() {
     cout << (4<<3) << endl;
     cout << (4>>1) << endl; // divide by 2^1
     cout << (32>>2) << endl;// divide by 2^2
+
+
+    int i = 2;
+    // preincrement and predecrement --i, ++i
+    cout << ++i << endl; // increment first than print = 3
+    cout << --i << endl; // decrement first than print = 2
+    // postincrement and postdecrement i--, i++
+    cout << i++ << endl; // print than increment, print 2 and i = 3 now
+    cout << i-- << endl; // print 3 than decrement so i = 2 now
+    cout << i << endl;
+
+    // ternary operator // if expr is true do left else do right.
+    // syntax expr ? expr_left : expr_right;
+    i == 2 ? cout<<true<<endl : cout<<false<<endl; // prints 1
+    i == 4 ? cout<<true<<endl : cout<<false<<endl; // prints 0
+    cout<< endl;
+    // sizeof operator : to find size of object in bytes >> amt of ram occupied
+    // sizeof is not a function, it is operator only.
+    cout << sizeof(bool) << endl; // 1
+    cout << sizeof(char) << endl; // 1
+    cout << sizeof(short) << endl; // 2
+    cout << sizeof(int) << endl; // 4
+    cout << sizeof(float) << endl; // 4
+    cout << sizeof(double) << endl;// 8
+    cout << sizeof(long double) << endl; // 16
+    cout << sizeof(long int) << endl; // 4
+    cout << sizeof(long long int) << endl; // 8
+
+    cout << sizeof(Base) <<endl; // 1 -- no memeber in class, so 1 byte given to identify diff objects
+    cout << sizeof(Base2) << endl; // int member so 4 bytes
+    cout << sizeof(Base3) << endl; // 12 due to structural padding. 4 + 1 + 1 + 4 = 10 only, but due to packing 4 + 4 + 4
+
+    char arr[] = {'a', 'a', 'b', 'c', 'd'};
+    cout << sizeof(arr) << endl;
+
+    string str = "hello";
+    cout << sizeof(str) << endl; // as string is object here.
+
+    char s[] = "hello"; // c style string
+    cout << sizeof(s) << endl; // 5 len + null char
+
+    fun(arr); // as arr is declared here only, it is not known to function fun, thus size is not proper.
+    // above is the reason we provide array size also as argument to function.
+    // thus above prints size of (char*)
+    cout << sizeof(char*) << endl;
+
+    // heap memory
+    int *ptr = new int(10);
+    cout << sizeof(ptr) << endl; // for 64 bit system.
+    delete ptr; // to avoid memory leak
     return 0;
 }

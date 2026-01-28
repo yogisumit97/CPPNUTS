edd08c3e26f7cdff726dca4a8e02cec49480de52
Sumit yogi <yogisumit97@gmail.com>
2026-01-28 10:21:37 +0530

    C++ Learning

--- 7_Arithmetic.cpp
+++ 7_Arithmetic.cpp
@@ -18,9 +18,15 @@ int main() {
     // assignment operator
 
     int a = 10, b = 20, c = 30;
+    if (a==b) { // equality operator
+        cout << "equal" << endl;
+    }
     a = b = c;
     cout << a << endl;
     cout << b << endl;
     cout << c << endl;
+    if (a==b) { // equality operator
+        cout << "equal" << endl;
+    }
     return 0;
 }
